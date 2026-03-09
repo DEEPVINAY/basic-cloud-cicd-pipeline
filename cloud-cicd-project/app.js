dbdc68ad9855c1a4a93db3aa636cb90695eefd73
Deep Vinay Parisarla <deepvinayparisarla@Deeps-MacBook-Pro.local>
2026-03-09 23:12:00 +0530

    test pipeline

--- cloud-cicd-project/app.js
+++ cloud-cicd-project/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Cloud CI/CD Pipeline Working!");
+    res.send("CI/CD Pipeline Successfully Deployed!");
 });
 
 app.listen(8080, () => {
